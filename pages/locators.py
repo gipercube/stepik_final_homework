f12b257722679d0f5da7c8768c54abd1be2081d7
gipercube <a.n.kozelskikh@gmail.com>
2022-09-07 22:42:38 +0300

    Получение текущего URL

--- pages/locators.py
+++ pages/locators.py
@@ -1,10 +1,10 @@
 from selenium.webdriver.common.by import By
 
 class MainPageLocators():
-    LOGIN_LINK = (By.CSS_SELECTOR, "#registration_link")
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
 class LoginPageLocators():
-    PAGE_URL = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    PAGE_URL = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
 
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     LOGIN_USER_EMAIL = (By.CSS_SELECTOR, "#id_login-username")
